c1c9cad3864ebca4c761d450da353485f9d43578
lukasschou <lukasschou@gmail.com>
2024-01-04 14:56:52 +0100

    ex10

---
+++ Exercises 9-13/Ex9/Ex9.ino
@@ -0,0 +1,39 @@
+int val = 0;
+float voltage = 0;
+float celscius = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop() {
+  delay(1000);
+
+  val = analogRead(A0);
+  voltage = 5./1023.*val;
+  celscius = voltage * 100;
+  Serial.println(val);
+  Serial.print(voltage);
+  Serial.println("V");
+  Serial.print(celscius);
+  Serial.println("°C");
+
+  if (celscius < 21){
+    digitalWrite(4, HIGH);
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+  }
+  else if (celscius > 28){
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  }
+  else {
+    digitalWrite(3, HIGH);
+    digitalWrite(2, LOW);
+    digitalWrite(4, LOW);
+  }
+}
