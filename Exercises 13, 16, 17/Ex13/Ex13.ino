1468c30f584f06ca9285d4bbe74e9cb338d35e07
lukasschou <lukasschou@gmail.com>
2024-01-05 10:40:33 +0100

    Update Ex13.ino

--- Exercises 13, 16, 17/Ex13/Ex13.ino
+++ Exercises 13, 16, 17/Ex13/Ex13.ino
@@ -18,7 +18,7 @@ void setup() {
 
 void loop() {
   if (counter_s < 10) { // stop playing the game after 10 succeses
-    // Sets the speed
+    // Sets the speed of the game (the speed of the LEDs)
     I += 1;
     if (I == 10000 - (counter_s * 750)) {
       x += 1;
@@ -31,6 +31,7 @@ void loop() {
     digitalWrite(9 + x, HIGH);
     Button = digitalRead(2);
 
+    // button pressed on the wrong time
     if (x != 2 && Button == 0) {
       counter_f += 1;
       Serial.println("Miss");
@@ -42,13 +43,15 @@ void loop() {
       digitalWrite(11, LOW);
       digitalWrite(12, LOW);
       digitalWrite(13, LOW);
-      delay(2000);
+      delay(1000);
+    // button pressed on the right time
     } else if (x == 2 && Button == 0) {
       counter_s += 1;
       Serial.println("Succes");
       Serial.print("you have ");
       Serial.print(counter_s);
       Serial.println(" succeses");
+      // makes the LED flash 8 times
       for (int i = 0; i <= 7; i++) {
         digitalWrite(9, HIGH);
         digitalWrite(10, HIGH);
@@ -63,24 +66,27 @@ void loop() {
         digitalWrite(13, LOW);
         delay(200);
       }
+      // reset cycle 
       x = 0;
       I = 0;
     }
 
+    // LEDs flash randomly when 8 succeses have been made
     if (counter_s == 8 && dance == 0) {
       for (int i = 0; i <= 14; i++) {
-        int ran = random(9, 14);
+        int ran = random(9, 14); // random port between 9-13
         digitalWrite(ran, HIGH);
         delay(100);
         digitalWrite(ran, LOW);
         delay(100);
       }
-      dance = 1;
+      dance = 1;  // makes the random flash happend only ones
       delay(500);
     }
 
-    digitalWrite(9 + x, LOW);
+    digitalWrite(9 + x, LOW); // turns the LED in the cycle off
 
+    // end of game
     if (counter_s == 10) {
       Serial.println("GAME OVER");
       Serial.print("You ended the game with ");
