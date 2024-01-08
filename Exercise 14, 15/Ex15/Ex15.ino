191943308e4db86e17f78ac1d34551a3901b3bb9
lukasschou <lukasschou@gmail.com>
2024-01-08 12:30:47 +0100

    Create Ex15.ino

---
+++ Exercise 14, 15/Ex15/Ex15.ino
@@ -0,0 +1,46 @@
+#include <ESP8266WiFi.h>
+#include <ThingSpeak.h>
+
+const char* ssid = "Lukas telefon";
+const char* pass = "11111111";
+
+WiFiClient client;
+
+unsigned long channelID = 2397414;        //your TS channal
+const char* APIKey = "7AK685PVDZJP7CLW";  //your TS API
+const char* server = "api.thingspeak.com";
+const int postDelay = 20 * 1000;  //post data every 20 seconds
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, INPUT);
+  WiFi.begin(ssid, pass);
+}
+
+float data;  //measured data
+
+void loop() {
+  digitalWrite(D2, LOW);
+  if (digitalRead(D2) == 1) {
+    data = 1;
+  }
+  else {
+    data = 0;
+  }
+
+  long rssi = WiFi.RSSI();
+
+  ThingSpeak.begin(client);
+  client.connect(server, 80);  //connect(URL, Port)
+
+  ThingSpeak.setField(1, data);               //set data on the X graph
+  ThingSpeak.setField(2, rssi);               //set data on the X graph
+  ThingSpeak.writeFields(channelID, APIKey);  //post everything to TS
+
+  //ThingSpeak.setField(2, data, rssi);               //set data on the X graph
+  //ThingSpeak.writeFields(channelID, APIKey);  //post everything to TS
+
+  client.stop();
+  delay(postDelay);  //wait and then post again
+}
